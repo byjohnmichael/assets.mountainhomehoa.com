4ff3c8cd8107c4b962131837c6e19c0e6166a281
John Michael <jm@byjohnmichael.com>
2026-06-29 16:33:34 -0600

    CORS update

--- api/verify.js
+++ api/verify.js
@@ -1,16 +1,7 @@
 const { get } = require('@vercel/edge-config');
 
-const ALLOWED_ORIGINS = [
-  'https://mountainhomehoa.com',
-  'https://www.mountainhomehoa.com',
-  'http://localhost:4000',
-  'http://localhost:3000',
-];
-
 module.exports = async function handler(req, res) {
-  const origin = req.headers['origin'] || '';
-  const allowed = ALLOWED_ORIGINS.includes(origin) ? origin : ALLOWED_ORIGINS[0];
-  res.setHeader('Access-Control-Allow-Origin', allowed);
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') return res.status(200).end();
